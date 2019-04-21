71c023e0f8bce714a91e4398190e399b52935e7e
furixturi <alabebop@gmail.com>
2019-04-21 14:40:47 +0900

    Add Material UI
    - Install material-ui core
    - Install typeface roboto and import in index.tsx
    - Use responsive meta viewport tag
    - Add google font roboto link in index.html
    - Use Typography component

--- app-idea-01-notes-react/src/modules/App/index.tsx
+++ app-idea-01-notes-react/src/modules/App/index.tsx
@@ -1,31 +1,27 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { Component } from 'react';
 import logo from '../../assets/logo.svg';
 import styles from './App.module.scss';
+
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
+        <CssBaseline />
         <header className={styles.AppHeader}>
-          <h2>Welcome to React</h2>
-          <img
-            src={logo}
-            className={styles.AppLogo}
-            alt="logo"
-          />
-          <p>
-            Edit <code>src/App.tsx</code> and save to
-            reload.
-          </p>
-          <a
-            className={styles.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Typography component="h1" variant="h2" gutterBottom>
+            Markdown Notes - React
+            <img
+              src={logo}
+              className={styles.AppLogo}
+              alt="logo"
+            />
+          </Typography>
         </header>
+        <div className={styles.NoteContainer}>
+        </div>
       </div>
     );
   }
