a308301f3a41d113b37770c1195fc86b36736fb6
furixturi <alabebop@gmail.com>
2019-04-22 21:00:09 +0900

    Add editing possibility
    - Add multiline TextInput
    - Use useState hook to sync text edit and display result
    - Use useState hook to toggle edit mode and display mode

--- app-idea-01-notes-react/src/modules/Note/index.tsx
+++ app-idea-01-notes-react/src/modules/Note/index.tsx
@@ -1,20 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
+import {
+  Card,
+  Typography,
+  WithStyles,
+  withStyles,
+  TextField
+} from '@material-ui/core';
+import React, { useState } from 'react';
+import styles from './styles';
 
-function Note() {
+interface Props extends WithStyles<typeof styles> {}
+
+function Note(props: Props) {
   const [mdNoteSrc, setMdNoteSrc] = useState(
     'Click to start writing'
   );
 
+  const [editing, setEditing] = useState(false);
+
+  const { classes } = props;
+
   return (
-    <div>
-      <div>
-        <Typography variant="body1" gutterBottom>
+    <Card className={classes.Card}>
+      {!editing && (
+        <Typography
+          variant="body1"
+          gutterBottom
+          onClick={() => setEditing(true)}
+        >
           {mdNoteSrc}
         </Typography>
-      </div>
-    </div>
+      )}
+      {editing && (
+        <TextField
+          multiline
+          value={mdNoteSrc}
+          onChange={e => {
+            setMdNoteSrc(e.target.value);
+          }}
+          onBlur={() => {
+            setEditing(false);
+          }}
+        />
+      )}
+    </Card>
   );
 }
 
-export default Note;
+export default withStyles(styles)(Note);
--- app-idea-01-notes-react/src/modules/Note/styles.ts
+++ app-idea-01-notes-react/src/modules/Note/styles.ts
@@ -2,7 +2,10 @@ import theme from '../../styles-theme/theme';
 import { Theme, createStyles } from '@material-ui/core';
 
 const styles = (theme: Theme) => createStyles({
-
+  Card: {
+    minWidth: 275,
+    maxWidth: 640
+  }
 });
 
 export default styles(theme);
